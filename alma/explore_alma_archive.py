51dafe874b6cfea756edd17d468d6801b4e1775f
richardgmcmahon <rgm@ast.cam.ac.uk>
2016-08-09 23:35:15 +0100

    added wget for archive log 	modified:   alma/explore_alma_archive.cfg 	modified:   alma/explore_alma_archive.py

--- alma/explore_alma_archive.py
+++ alma/explore_alma_archive.py
@@ -1,11 +1,11 @@
 """
 
-
-
 astroquery did not work
 http://astroquery.readthedocs.io/en/latest/
 
 so used alma_aq_data.csv via wget
+
+
 
 """
 from __future__ import (absolute_import, division, print_function,
@@ -31,12 +31,43 @@ from astropy.coordinates import match_coordinates_sky
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-
 from astropy import units as u
 from astropy.table import Table
 from astropy.table import vstack
 
 from astroquery.alma import Alma
+
+
+def wget_archivelog():
+    """
+    get a full list of everything ALMA has observed as csv file
+
+    How can I get a full list of everything ALMA has observed?
+    https://help.almascience.org/snoopi/index.php?/Knowledgebase/Article/View/308
+
+    wget -O alma_aq_data.csv --no-check-certificate "http://almascience.org/aq/?result_view=raw&science_observations=&download=true&format=CSV"
+
+    """
+
+    import os
+    import time
+
+    now = time.localtime(time.time())
+    datestamp = time.strftime("%Y%m%d",now)
+
+    output = 'alma_aq_data_' + datestamp + '.csv'
+    wget_options = ' --no-check-certificate '
+
+    url_prefix = 'http://almascience.org/'
+
+    url_suffix = 'aq/?result_view=raw&science_observations=&download=true&format=CSV'
+
+    wget_url = url_prefix + url_suffix
+
+    cmd = 'wget ' + wget_options + ' "' + wget_url + '" ' + ' -O ' + output
+
+    print('Executing:', cmd)
+    os.system(cmd)
 
 
 def rd_config(config_file=None):
@@ -52,7 +83,8 @@ def rd_config(config_file=None):
     print('__name__: ', __name__)
 
     # could get this from __file__ search in a few default places
-    config_file = 'explore_alma_archive.cfg'
+    if config_file is None:
+        config_file = 'explore_alma_archive.cfg'
 
     config = configparser.RawConfigParser()
     config.read(config_file)
@@ -60,7 +92,6 @@ def rd_config(config_file=None):
     print(config)
 
     return config
-
 
 def ScanIntent(data, debug=False):
 
@@ -107,7 +138,6 @@ def Integration(data, debug=False):
     plt.savefig(plotfile)
     plt.clf()
 
-
     itest = xdata < 3600
     xdata = xdata[itest]
 
@@ -135,7 +165,7 @@ def Largest_angular_scale(data):
     return
 
 
-def Frequency_support(data, debug=False):
+def Frequency_support(data, debug=False, infile=None):
     """
 
     decode the frequency metadata
@@ -221,6 +251,32 @@ def Frequency_support(data, debug=False):
     plt.legend()
     plt.xlabel('Observed Frequency (Ghz)')
     plt.ylabel('Frequency per bin')
+    plt.title(infile)
+
+    plotfile = infile + '_Frequency_support_fig1.png'
+    print('Saving:', plotfile)
+    plt.savefig(plotfile)
+    plt.clf()
+
+
+
+    xdata = 300.0/np.asarray(xdata)
+    plt.hist(xdata, bins=100, label=label)
+    plt.grid()
+    plt.legend()
+    # plt.xlabel('Observed Frequency (Ghz)')
+    plt.ylabel('Frequency per bin')
+    plt.title(infile)
+
+    # axes = plt.gca()
+    # xdata = np.asarray(axes.get_xlim())
+    # ydata = np.asarray(axes.get_ylim())
+    # print(xdata)
+    # print(ydata)
+
+    # plt.twiny()
+    plt.xlabel("Observed Wavelength (mm)")
+    # plt.plot(300.0/xdata, ydata)
 
     plotfile = infile + '_Frequency_support_fig2.png'
     print('Saving:', plotfile)
@@ -321,13 +377,16 @@ def parse_args(version=None):
     parser.add_argument("--debug", action='store_true', default=False,
                         dest='debug', help="debug option; logging level DEBUG")
 
+    parser.add_argument("--verbose", action='store_true', default=False,
+                        dest='verbose', help="verbose option; logging level DEBUG")
+
     parser.add_argument(
-        '--ramax', action='store', default=24.0,
-        help='Tile centre maximum  RA')
+        '--read_fits', action='store_true', default=True,
+        help='Read the fits format file')
 
     parser.add_argument(
         '--read_csv', action='store_true', default=False,
-        help='Read the csv file')
+        help='Read the csv format file')
 
     parser.add_argument(
         '--convert_csv', action='store_true', default=False,
@@ -335,14 +394,19 @@ def parse_args(version=None):
 
     parser.add_argument(
         '--write_fits', action='store_true', default=False,
-        help='Write fits format ALMA archive file')
+        help='Write fits format ALMA archive file after ')
+
+    parser.add_argument(
+        '--wget_csvfile', action='store_true', default=False,
+        help='run wget to get new copy of observation log in csv format')
 
 
     args = parser.parse_args()
 
     return args
 
-def plot_radec(ra, dec, suffix=None):
+
+def plot_radec(ra, dec, title=None, suffix=None, infile=None):
     """
 
     """
@@ -350,8 +414,8 @@ def plot_radec(ra, dec, suffix=None):
     xdata = ra
     ydata = dec
 
-    print(np.min(xdata), np.max(xdata), len(xdata))
-    print(np.min(ydata), np.max(ydata), len(ydata))
+    print('xdata range:', np.min(xdata), np.max(xdata), len(xdata))
+    print('ydata range:', np.min(ydata), np.max(ydata), len(ydata))
     ndata = len(xdata)
     plt.title(infile)
     plt.scatter(xdata, ydata, s=2, edgecolor='none', label=str(ndata))
@@ -360,7 +424,9 @@ def plot_radec(ra, dec, suffix=None):
     plt.ylim(-90.0, 60.0)
     plt.legend()
 
-    plotfile = './plots/infile' + '_radec.png'
+    if title is not None: plt.title(title)
+    plotfile = 'radec.png'
+    if infile is not None: plotfile = infile + '_radec.png'
     plt.savefig(plotfile)
     plt.clf()
 
@@ -374,7 +440,6 @@ if __name__ == '__main__':
     then csv file in that order and print out the help info
 
     """
-
 
     # could part of debug option
     print('numpy.__version__:', np.__version__)
@@ -394,29 +459,43 @@ if __name__ == '__main__':
     t0 = time.time()
 
     args = parse_args(version=None)
-    config = rd_config()
+
+    read_csv = args.read_csv
+    read_fits = args.read_fits
+
+    write_fits = args.write_fits
+    convert_csv = args.convert_csv
+    wget_csvfile = args.wget_csvfile
 
     debug = args.debug
-    read_csv = args.read_csv
-    write_fits = args.write_fits
-    read_fits = True
 
-    rd_config(config_file=None)
+    config = rd_config(config_file=None)
+    config = rd_config()
 
-    if read_csv:
-        infile = 'alma_aq_data.csv'
+    infile_prefix_aa = config.get("defaults", "infile_prefix_aa")
+    print('infile_prefix_aa:', infile_prefix_aa)
+
+    if wget_csvfile:
+        wget_archivelog()
+        sys.exit()
+
+    if read_csv or convert_csv:
+        infile = infile_prefix_aa + '.csv'
         print('Read:', infile)
 
         table = Table.read(infile, format='ascii')
+        table.meta['file'] = infile
         table.info('stats')
 
-    if write_fits:
-        table.write('alma_aq_data.fits', overwrite=True)
-
+    if write_fits or convert_csv:
+        outfile = infile_prefix_aa + '.fits'
+        table.write(outfile, overwrite=True)
 
     if read_fits:
-        infile = 'alma_aq_data.fits'
+        infile = infile_prefix_aa + '.fits'
+        print('Reading:', infile)
         table = Table.read(infile)
+        table.meta['file'] = infile
         print('Elapsed time(secs):', time.time() - t0)
         table.info('stats')
         colnames = table.colnames
@@ -431,14 +510,16 @@ if __name__ == '__main__':
         if debug:
             key=raw_input("Enter any key to continue: ")
 
-    plot_radec(table['RA'], table['Dec'])
+    plot_radec(table['RA'], table['Dec'],
+               title=table.meta['file'], infile=infile)
 
     Largest_angular_scale(data=table['Largest angular scale'])
 
     if debug:
         key=raw_input("Enter any key to continue: ")
 
-    Frequency_support(data=table['Frequency support'], debug=debug)
+    Frequency_support(data=table['Frequency support'], infile=infile,
+                      debug=debug)
 
     Integration(data=table['Integration'])
 
@@ -465,9 +546,10 @@ if __name__ == '__main__':
     #                       nthneighbor=1, storekdtree='_kdtree_sky')
     # Finds the nearest on-sky matches of a coordinate or coordinates in
     # a set of catalog coordinates.
+    print('Using: match_coordinates_sky')
     idx, d2d, d3d = match_coordinates_sky(radec_alma, radec_veron)
 
-    print()
+    print('len(idx):', len(idx))
     print('idxmatch range:', np.min(idx), np.max(idx))
     idx_unique, idx_unique_indices = np.unique(idx, return_index=True)
     print('Number of unique sources:', len(idx_unique), len(idx))
@@ -478,8 +560,10 @@ if __name__ == '__main__':
     # match_to_catalog_sky(catalogcoord[, nthneighbor])
     # Finds the nearest on-sky matches of this coordinate in a set
     # of catalog coordinates.
+    print('Using: match_to_catalog_sky')
     idx, d2d, d3d = radec_veron.match_to_catalog_sky(radec_alma)
 
+    print('len(idx):', len(idx))
     print('idxmatch range:', np.min(idx), np.max(idx))
     idx_unique, idx_unique_indices = np.unique(idx, return_index=True)
     print('Number of unique sources:', len(idx_unique), len(idx))
@@ -492,16 +576,19 @@ if __name__ == '__main__':
     # create table of Veron quasars that have ALMA observations
     veron_alma = Veron2010[idx_unique_indices]
     xdata = d2d[idx_unique_indices].arcsec
-    itest = xdata  < 30.0
+    itest = xdata  < 15.0
 
     veron_alma = veron_alma[itest]
     ndata = len(veron_alma)
-    print('Number of Veron+2010 Quasars within 30" match radius:', ndata)
+    print('Number of Veron+2010 Quasars within 15" match radius:', ndata)
     print()
+
+    if debug:
+        key=raw_input("Enter any key to continue: ")
 
     xdata = xdata[itest]
     ndata = len(xdata)
-    print('Number of Veron+2010 Quasars within 30" match radius:', ndata)
+    print('Number of Veron+2010 Quasars within 15" match radius:', ndata)
 
     plt.hist(xdata, bins=60, label=str(ndata))
     plt.grid()
@@ -510,7 +597,7 @@ if __name__ == '__main__':
     plt.xlabel('Separation (")')
     plt.ylabel('Frequency per bin')
 
-    plotfile = infile + '_fig2.png'
+    plotfile = infile + '_Separation_hist.png'
     print('Saving:', plotfile)
     plt.savefig(plotfile)
     plt.clf()
@@ -607,6 +694,9 @@ if __name__ == '__main__':
     print(len(itest_rqq))
     print(len(veron_alma[itest_rqq]))
 
+    veron_alma[itest_rqq].info('stats')
+    ndata = len(veron_alma)
+
     igood = 0
     for i in range(0, ndata):
         if (veron_alma[i]['z'] >= 0.3 and
@@ -619,7 +709,7 @@ if __name__ == '__main__':
                   veron_alma[i]['F6cm'], veron_alma[i]['F20cm'])
 
     #
-    veron_alma[itest_rqq].info('stats')
+
     xdata = veron_alma[itest_rqq]['RA']
 
     # read and match to SDSS DR7QSO
